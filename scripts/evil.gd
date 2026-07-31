8b8dbaed8b63d97efdf1361b0d43b618f7814b62
kat <katwang7749@gmail.com>
2026-07-31 19:02:14 -0400

    enemies spawn more w time and move towards player

--- scripts/evil.gd
+++ scripts/evil.gd
@@ -1,28 +1,53 @@
 extends CharacterBody2D
 
 @export var health = randi_range(4, 10)
+@export var speed: float = 50.0
 @export var hurt_text = load("res://scenes/hurt-text.tscn")
 
+var player: Node2D = null
+var wander_timer: float = 0.0
+var random_offset: Vector2 = Vector2.ZERO
 
-func onready() -> void:
-	pass
+func _ready() -> void:
+	# Find the player using the "player" group
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player = players[0]
 
-func _physics_process(_delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if not is_instance_valid(player):
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
+	wander_timer -= delta
+	if wander_timer <= 0:
+		wander_timer = randf_range(1.0, 2.0)
+		random_offset = Vector2(randf_range(-150, 150), randf_range(-150, 150))
+
+	var target_pos = player.global_position + random_offset
+	var direction = global_position.direction_to(target_pos)
+	
+	velocity = direction * speed
+	move_and_slide()
+	
+	if velocity.x < 0:
+		$Evil.flip_h = true
+	elif velocity.x > 0:
+		$Evil.flip_h = false
 	
 func take_damage(amount: int) -> void:
-		health = health - amount
+	health = health - amount
+	
+	if health <= 0:
+		die()
 		
-		if health <= 0:
-			die()
-			
-		add_hurt_text(amount)
-		$AnimationPlayer.play("hit")		
-		$Evil.play("hit")
-		await $Evil.animation_finished
-		$Evil.play("default")
+	add_hurt_text(amount)
+	$AnimationPlayer.play("hit")		
+	$Evil.play("hit")
+	await $Evil.animation_finished
+	$Evil.play("default")
 		
-
 func die() -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
 	$AnimationPlayer.play("die")
@@ -36,4 +61,4 @@ func add_hurt_text(dmg):
 	hurt_text_instance.add_theme_color_override("default_color", Color(0.117647, 0.235294, 0.556863))
 	hurt_text_instance.text = str(dmg)
 	hurt_text_instance.global_position.y = global_position.y - randi_range(55, 70)
-	hurt_text_instance.global_position.x = global_position.x + randi_range(12, 25)	
+	hurt_text_instance.global_position.x = global_position.x + randi_range(12, 25)
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,25 +1,38 @@
 extends Node2D
 
 @onready var timer = $Timer
-@export var time_left: float = 6.0
+@onready var gunTimer = $Timer2
+
+@export var time_left: float = randf_range(5.0, 6.5)
+@export var min_time_left: float = 2.0
+@export var spawn_acceleration: float = 0.2
+
+@export var gun_time_left: float = 15.0
 @export var opp = load("res://scenes/evil.tscn")
+@export var gun = load("res://scenes/dropped-gun.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.wait_time = time_left
+	timer.start() # Ensure timer starts right away
 	spawnOpp()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if timer.is_stopped():
-			spawnOpp()
-			timer.start()
- 
+		spawnOpp()
+		
+		timer.wait_time = max(min_time_left, timer.wait_time - spawn_acceleration)
+		timer.start()
+
 func spawnOpp() -> void:
 	var opp_instance = opp.instantiate()
 	get_tree().current_scene.add_child(opp_instance)
 	
 	var camera = get_viewport().get_camera_2d()
+	if not camera:
+		return
+		
 	var camera_position = camera.global_position
 	
 	var random_position = Vector2(
@@ -27,5 +40,4 @@ func spawnOpp() -> void:
 		randf_range(camera_position.y - 250, camera_position.y + 250)
 	)
 
-	opp_instance.global_position = random_position	
-	
+	opp_instance.global_position = random_position
