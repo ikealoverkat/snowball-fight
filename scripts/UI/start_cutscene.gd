d368941b656b4363feb7e8131b3e6818a6accf74
kat <katwang7749@gmail.com>
2026-07-31 23:10:08 -0400

    add placeholder cutscene

--- scripts/UI/start_cutscene.gd
+++ scripts/UI/start_cutscene.gd
@@ -1,15 +1,11 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	$Animated.play("default")
+	await $Animated.animation_finished
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/UI/start_cutscene.tscn")
